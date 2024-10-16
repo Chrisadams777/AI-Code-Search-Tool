d08e2c6249b91e617998717a32b900eacdea504c
Christopher Adams <christopherallenadams@hotmail.com>
2024-10-15 18:22:36 -0700

    Update db.go

--- backend/db/db.go
+++ backend/db/db.go
@@ -1,1 +1,29 @@
+// db.go - Database connection initialization.
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func InitDB() {
+	connStr := os.Getenv("DB_CONNECTION_STRING")
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping the database: %v", err)
+	}
+
+	fmt.Println("Database connection initialized")
+}
