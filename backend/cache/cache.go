c11ddd0422d8ef05154f93f90d58caa8b3847ba1
Christopher Adams <christopherallenadams@hotmail.com>
2024-10-15 18:23:21 -0700

    Update cache.go

--- backend/cache/cache.go
+++ backend/cache/cache.go
@@ -1,1 +1,26 @@
+// cache.go - Redis cache initialization.
 package cache
+
+import (
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"context"
+	"os"
+)
+
+var rdb *redis.Client
+
+func InitRedis() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"), // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := rdb.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Printf("Failed to connect to Redis: %v\n", err)
+	} else {
+		fmt.Println("Redis cache initialized")
+	}
+}
